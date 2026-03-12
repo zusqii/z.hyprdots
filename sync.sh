323f2439b974f572d1e511b5cf9517055530c05b
zusqii <zusqii.99@gmail.com>
2026-03-12 16:25:14 +0100

    Update dots: 2026-03-12 16:25

--- sync.sh
+++ sync.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
 
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃                                                                  ┃
-# ┃      ZUSQII  -  M A S T E R   D O T   S Y N C                    ┃
-# ┃                                                                  ┃
-# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
 REPO_DIR="/home/zusqii/z.hyprdots"
 CONF_DIR="$HOME/.config"
 
-echo "🚀 Starting backup..."
 mkdir -p "$REPO_DIR"/.config
 
-items=(cava clock-rs fastfetch hypr kitty rofi swaync waybar wayclick wlogout)
+items=(cava clock-rs fastfetch hypr kitty matugen rofi swaync waybar wayclick wlogout)
 
 for item in "${items[@]}"; do
     if [ -d "$CONF_DIR/$item" ]; then
         cp -r "$CONF_DIR/$item" "$REPO_DIR/.config/"
-        echo "✅ Copied .config/$item"
+        echo "✅ Copied $item"
     else
-        echo "⚠️  Warning: $item not found in .config"
+        echo "⚠️  Missing $item"
     fi
 done
 
@@ -28,19 +21,14 @@ cp -r "$HOME/.scripts" "$REPO_DIR/"
 cp -r "$HOME/.themes" "$REPO_DIR/"
 cp -r "$HOME/Pictures/Wallpapers" "$REPO_DIR/"
 
-echo "✅ Copied .zshrc, .scripts, .themes, and Wallpapers"
-
-echo "📤 Pushing to GitHub..."
 cd "$REPO_DIR" || exit
 
-# Add all changes
 git add .
 
-# Only commit if there are changes to avoid error messages
 if git diff-index --quiet HEAD --; then
-    echo "✨ No changes detected. Repository is already up to date."
+    echo "✨ No changes to push."
 else
-    git commit -m "Update dots & wallpapers: $(date +'%Y-%m-%d %H:%M')"
+    git commit -m "Update dots: $(date +'%Y-%m-%d %H:%M')"
     git push origin main
-    echo "🎉 Everything successfully pushed to GitHub!"
+    echo "🎉 Pushed to GitHub!"
 fi
